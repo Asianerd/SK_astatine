13659a45984e904d46743f12a41ae7d5423848c3
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2023-05-27 17:41:43 +0800

    Rename + misc

--- general_usage/temp.php
+++ general_usage/temp.php
@@ -7,8 +7,18 @@ foreach ($user_db->query("SELECT user_id FROM `user`") as $rows) {
 ?> -->
 
 <?php
-var_dump(explode("i", "abcdef"));
+// var_dump(explode("i", "abcdef"));
 
+$highest_user_id = 0;
+
+$user_db = new mysqli($hostname='localhost', $username='astatine', $password='password', $database='astatine_data');
+
+$result = $user_db->query("SELECT MAX(user_id)
+FROM pelanggan;");
+
+foreach ($result as $element) {
+    $highest_user_id = $element["MAX(user_id)"];
+}
 
 
 ?>
